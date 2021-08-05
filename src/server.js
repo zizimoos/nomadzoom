cc65d60bd919d3e9656d90da85556589431405b0
henry <zizimoos@gmail.com>
2021-08-06 00:50:17 +0900

    stun server

--- src/server.js
+++ src/server.js
@@ -14,5 +14,21 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const ioServer = SocketIO(httpServer);
 
+ioServer.on("connection", (socket) => {
+  socket.on("join_room", (roomname) => {
+    socket.join(roomname);
+    socket.to(roomname).emit("welcome");
+  });
+  socket.on("offer", (offer, roomname) => {
+    socket.to(roomname).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomname) => {
+    socket.to(roomname).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomname) => {
+    socket.to(roomname).emit("ice", ice);
+  });
+});
+
 const handleListen = () => console.log("✅ Listening on http://localhost:3000");
 httpServer.listen(3000, handleListen);
